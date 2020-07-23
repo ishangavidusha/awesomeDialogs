3a9fdf0fd0082e4a6074589dec0c62e4a221a512
unknown <ishangavidusha@gmail.com>
2020-07-23 18:18:38 +0900

    Fix Web Issue With Dialog Width

--- lib/src/vertical_stack_header_dialog.dart
+++ lib/src/vertical_stack_header_dialog.dart
@@ -34,82 +34,87 @@ class VerticalStackDialog extends StatelessWidget {
         alignment: aligment,
         child: Stack(
           children: <Widget>[
-            Container(
-              padding: isDense
-                  ? EdgeInsets.only(
-                      top: 65.0, left: 15.0, right: 15.0, bottom: 10.0)
-                  : EdgeInsets.only(
-                      top: 65.0, left: 40.0, right: 40.0, bottom: 10.0),
-              child: Material(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10.0)),
-                elevation: 0.5,
-                color: Theme.of(context).cardColor,
-                child: Container(
-                  padding: padding,
-                  width: MediaQuery.of(context).size.width,
-                  child: SingleChildScrollView(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisSize: MainAxisSize.min,
-                      children: <Widget>[
-                        SizedBox(
-                          height: header != null ? 50.0 : 15,
-                        ),
-                        body ??
-                            Column(
-                              mainAxisSize: MainAxisSize.min,
-                              children: <Widget>[
-                                Text(
-                                  title,
-                                  textAlign: TextAlign.center,
-                                  style: Theme.of(context).textTheme.headline6,
-                                ),
-                                SizedBox(
-                                  height: 10.0,
-                                ),
-                                Flexible(
-                                  fit: FlexFit.loose,
-                                  child: SingleChildScrollView(
-                                    physics: BouncingScrollPhysics(),
-                                    child: Text(
-                                      desc,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width > 500 ? 500 : MediaQuery.of(context).size.width,
+                  padding: isDense
+                      ? EdgeInsets.only(
+                          top: 65.0, left: 15.0, right: 15.0, bottom: 10.0)
+                      : EdgeInsets.only(
+                          top: 65.0, left: 40.0, right: 40.0, bottom: 10.0),
+                  child: Material(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0)),
+                    elevation: 0.5,
+                    color: Theme.of(context).cardColor,
+                    child: Container(
+                      padding: padding,
+                      child: SingleChildScrollView(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            SizedBox(
+                              height: header != null ? 50.0 : 15,
+                            ),
+                            body ??
+                                Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: <Widget>[
+                                    Text(
+                                      title,
                                       textAlign: TextAlign.center,
+                                      style: Theme.of(context).textTheme.headline6,
                                     ),
-                                  ),
+                                    SizedBox(
+                                      height: 10.0,
+                                    ),
+                                    Flexible(
+                                      fit: FlexFit.loose,
+                                      child: SingleChildScrollView(
+                                        physics: BouncingScrollPhysics(),
+                                        child: Text(
+                                          desc,
+                                          textAlign: TextAlign.center,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
                                 ),
-                              ],
+                            SizedBox(
+                              height: 16.0,
                             ),
-                        SizedBox(
-                          height: 16.0,
+                            if (btnOk != null || btnCancel != null)
+                              Container(
+                                padding: EdgeInsets.symmetric(
+                                    vertical: 10, horizontal: 20),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: <Widget>[
+                                    if (btnCancel != null)
+                                      Expanded(
+                                        child: btnCancel ?? Container(),
+                                      ),
+                                    if (btnCancel != null && btnOk != null)
+                                      SizedBox(
+                                        width: 10,
+                                      ),
+                                    if (btnOk != null)
+                                      Expanded(
+                                        child: btnOk,
+                                      )
+                                  ],
+                                ),
+                              )
+                          ],
                         ),
-                        if (btnOk != null || btnCancel != null)
-                          Container(
-                            padding: EdgeInsets.symmetric(
-                                vertical: 10, horizontal: 20),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: <Widget>[
-                                if (btnCancel != null)
-                                  Expanded(
-                                    child: btnCancel ?? Container(),
-                                  ),
-                                if (btnCancel != null && btnOk != null)
-                                  SizedBox(
-                                    width: 10,
-                                  ),
-                                if (btnOk != null)
-                                  Expanded(
-                                    child: btnOk,
-                                  )
-                              ],
-                            ),
-                          )
-                      ],
+                      ),
                     ),
                   ),
                 ),
-              ),
+              ],
             ),
             if (header != null)
               Row(
